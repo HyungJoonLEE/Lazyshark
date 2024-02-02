8a06d294f3e7a91a13409f2982b10397b7d21549
HyungJoonLEE <itineris24@naver.com>
2024-02-02 13:58:52 -0800

    packet table

--- src/Analyzewindow.cpp
+++ src/Analyzewindow.cpp
@@ -8,22 +8,6 @@ AnalyzeWindow::AnalyzeWindow(QWidget *parent) :
 {
 //    size_t rowCount = m_av.size();
     ui->setupUi(this);
-//    ui->tableWidget->setRowCount(rowCount);
-//    for (int i = 0; i < rowCount; i++) {
-//        ui->tableWidget->setItem(i, 0, new QTableWidgetItem(QString::number(m_av[i]->getNo())));
-//        ui->tableWidget->setItem(i, 1, new QTableWidgetItem(QString("TEST TIME")));
-//        ui->tableWidget->setItem(i, 2, new QTableWidgetItem(QString::number(m_av[i]->getIpv4Hdr()->ip_p)));
-//        // Convert 'in_addr' to a C string
-//        const char* ipSrc = inet_ntoa(m_av[i]->getIpv4Hdr()->ip_src);
-//        const char* ipDst = inet_ntoa(m_av[i]->getIpv4Hdr()->ip_dst);
-//        ui->tableWidget->setItem(i, 3, new QTableWidgetItem(QString::fromLatin1(ipSrc)));
-//        ui->tableWidget->setItem(i, 4, new QTableWidgetItem(QString::number(m_av[i]->getTCPHdr()->th_sport)));
-//        ui->tableWidget->setItem(i, 5, new QTableWidgetItem(QString::number(m_av[i]->getTCPHdr()->th_dport)));
-//        ui->tableWidget->setItem(i, 6, new QTableWidgetItem(QString::fromLatin1(ipDst)));
-//        unsigned int plen = m_av[i]->getIpv4Hdr()->ip_len - m_av[i]->getIpv4Hdr()->ip_hl - m_av[i]->getTCPHdr()->doff * 4;
-//        ui->tableWidget->setItem(i, 7, new QTableWidgetItem(QString::number(plen)));
-//        ui->tableWidget->setItem(i, 8, new QTableWidgetItem(QString("TEST HELLO")));
-//    }
 }
 
 
